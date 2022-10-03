99524776caea8b1e747fb42ced75173e80f96798
radekgryglick <112925922+radekgryglick@users.noreply.github.com>
2022-10-03 12:17:00 +0200

    Add files via upload

---
+++ CW6/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) 
+{
+system("Chcp 1250");
+SetConsoleTitleA("¯ó³ta ³ódŸ podwodna");
+system("Color fc");
+cout<<"Wypuszcza b¹belki";
+cout<<endl;
+system("Pause");
+return 0;
+}
